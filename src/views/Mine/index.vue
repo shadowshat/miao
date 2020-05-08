b97ffcaa8d9aec467c819df34060b96e058820b8
shadowshat <1139177688@qq.com>
2020-05-08 10:44:07 +0800

    add componnets

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<div id="main">
+		
+		<Header />
+		
+		<div id="content">
+			<Login />
+		</div>
+		<tabBar />
+	</div>
+</template>
+
+<script>
+	import Header from '@/components/header/header.vue'
+	import tabBar from '@/components/tabBar/tabBar.vue'
+	import Login from '@/components/Login'
+	export default{
+		name:'mine',
+		components:{
+			Header,
+			tabBar,
+			Login
+		}
+	}
+</script>
+
+<style>
+</style>
